f39e63f3702c79bb910ee5015a90ac862035cca5
Samuel Gordalina <samuel.gordalina@gmail.com>
2020-07-30 14:03:49 -0700

    Bump version to v0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v0.2.0
+
+- Support Hush v0.2.0
+
 ## v0.1.1
 
 - Ensure ability to load config from parameter as well as runtime
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule HushGcpSecretManager.MixProject do
   use Mix.Project
 
-  @version "0.1.2"
+  @version "0.2.0"
   @source_url "https://github.com/gordalina/hush_gcp_secret_manager"
 
   def project do
