b49b5c178bb2e1c3980b5dc3aefd17117b46a0d1
Samuel Gordalina <samuel.gordalina@gmail.com>
2022-08-23 13:40:23 -1000

    Update tests

--- test/http_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule Hush.Provider.GcpSecretManager.HttpTest do
-  use ExUnit.Case
-  doctest Hush.Provider.GcpSecretManager.Http
-  alias Hush.Provider.GcpSecretManager
-
-  test "for_scope/1" do
-    assert_raise CaseClauseError, fn -> GcpSecretManager.Http.get("", []) end
-  end
-end
